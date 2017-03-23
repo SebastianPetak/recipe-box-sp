5671eb9b7ddf6d4d71fcecee04d4a9f7bad84d56
Sebastian Petak <sebastianpetak@gmail.com>
2017-03-23 18:47:53 -0500

    Add Styles to RecipesHeading Create a styles file in app/styles. Add Styles to RecipesHeading. Add a font to index.html from google fonts.

---
+++ app/styles/styles.js
@@ -0,0 +1,21 @@
+const styles = {
+	topBarStyles: {
+		width: '100%',
+		backgroundColor: '#FF6347',
+		color: '#FDF5E6'
+	},
+	topBarTitleStyles: {
+		marginLeft: '10%',
+		fontFamily: 'Signika Negative', /* sans-serif' */
+		fontWeight: '700',
+		whiteSpace: 'nowrap'
+	},
+	addRecipeButton: {
+		backgroundColor: '#FDF5E6',
+		color: 'black',
+		borderColor: '#FDF5E6',
+		/*whiteSpace: 'nowrap'*/
+	}
+};
+
+module.exports = styles;
