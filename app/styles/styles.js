a191b9db503423b42c9e22edd50619b2cf4224aa
Sebastian Petak <sebastianpetak@gmail.com>
2017-03-24 09:50:35 -0500

    Add Recipe Button Styles Modify styles.js to add styling to the 'Add Recipe' button.

--- app/styles/styles.js
+++ app/styles/styles.js
@@ -11,11 +11,22 @@ const styles = {
 		whiteSpace: 'nowrap'
 	},
 	addRecipeButton: {
-		backgroundColor: '#FDF5E6',
-		color: 'black',
+		backgroundColor: '#FF6347',
+		color: '#FDF5E6', //black: #191919
 		borderColor: '#FDF5E6',
+		marginRight: '3%',
+		fontFamily: 'Signika Negative',
+		fontSize: '1.5em',
+		border: '1px solid #FDF5E6',
+		borderRadius: '2%'
 		/*whiteSpace: 'nowrap'*/
 	}
 };
 
+/* Color pallet
+	tomato: #FF6347
+	neon-teal: #47e3ff
+	off-black: #191919
+	off-white: #FDF5E6
+*/
 module.exports = styles;
