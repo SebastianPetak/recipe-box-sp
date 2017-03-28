8ad88f9494577dae5957a738d721f9fa310afe5c
Sebastian Petak <sebastianpetak@gmail.com>
2017-03-28 16:24:10 -0500

    Refactor to Stateless Functional Components RecipeFooter.js RecipeList.js and Recipesheading.js do not have state and could be stateless functional components. Rewrite the components to be stateless functional components.

--- app/components/RecipeList.js
+++ app/components/RecipeList.js
@@ -1,15 +1,12 @@
 const React = require('react');
 const fullListDivStyles = require('../styles/styles').fullListDivStyles;
 
-module.exports = class RecipeList extends React.Component {
-	constructor(props) {
-		super(props);
-	}
-	render() {
-		return (
-			<div className='row' style={fullListDivStyles}>
-				{this.props.currentRecipes}
-			</div>
-		);
-	}
+const RecipeList = (props) => {
+	return (
+		<div className='row' style={fullListDivStyles}>
+			{props.currentRecipes}
+		</div>
+	);
 };
+
+module.exports = RecipeList;
--- app/components/RecipesHeading.js
+++ app/components/RecipesHeading.js
@@ -3,20 +3,17 @@ const topBarStyles = require('../styles/styles.js').topBarStyles;
 const addRecipeButton = require('../styles/styles.js').addRecipeButton;
 const topBarTitleStyles = require('../styles/styles.js').topBarTitleStyles;
 
-module.exports = class RecipesHeading extends React.Component {
-	constructor(props) {
-		super(props);
-	}
-	render() {
-		return (
-			<div className='row'>
-				<div className='top-bar' style={topBarStyles}>
-					<div className="top-bar-title">
-						<h1 style={topBarTitleStyles}>Recipe Box</h1>
-					</div>
-					<button className='button large' type='button' style={addRecipeButton}>Add Recipe</button>
+const RecipesHeading = (props) => {
+	return (
+		<div className='row'>
+			<div className='top-bar' style={topBarStyles}>
+				<div className="top-bar-title">
+					<h1 style={topBarTitleStyles}>Recipe Box</h1>
 				</div>
+				<button className='button large' type='button' style={addRecipeButton}>Add Recipe</button>
 			</div>
-		);
-	}
+		</div>
+	);
 };
+
+module.exports = RecipesHeading;
